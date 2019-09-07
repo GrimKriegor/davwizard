a06db51523d01bfa7c691f616eea142b557818e5
Grim Kriegor <grimkriegor@krutt.org>
2019-09-07 15:24:31 +0100

    Remove stale db lock files when syncing

--- davsync.sh
+++ davsync.sh
@@ -1,8 +1,11 @@
 #!/usr/bin/env bash
 
 davwizard_dir="$HOME/.config/davwizard"
+vdirsyncer_dir="$HOME/.vdirsyncer"
+
+find "$vdirsyncer_dir" -type f -name "*.items*" -mmin +59 -exec rm {} \;
 
 for account in $(ls -p $davwizard_dir/accounts/ | grep -v /); do
   echo "Syncing account $account"
-  vdirsyncer -c $davwizard_dir/accounts/$account.d/vdirsyncer.conf sync
+  vdirsyncer -c "$davwizard_dir/accounts/$account.d/vdirsyncer.conf" sync
 done
